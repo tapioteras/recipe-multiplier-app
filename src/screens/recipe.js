ff597d0c2099ec9755930c4e9992d4a951c0c418
Tapio Teras <tapio.teras@reaktor.com>
2021-07-10 12:05:15 +0300

    Implement header to recipe screen

--- src/screens/recipe.js
+++ src/screens/recipe.js
@@ -1,21 +1,23 @@
 import React, {useEffect, useState} from "react"
 import {Text, View} from "react-native";
 import {parseKRuokaRecipe} from "../utils/RecipeUtils";
+import {styles} from "./recipes";
+import Header from "../components/Header";
 
 const RecipeScreen = ({route}) => {
     const recipe = parseKRuokaRecipe(route.params.recipe)
 const { name, portions, ingredients, steps } = recipe
-    return (<View>
-<Text>{name}</Text>
+    return (<View style={styles.container}>
+<Header>{name}</Header>
         <Text>{portions} annosta</Text>
         <View style={{ paddingTop: 20 }}>
-        <Text>Ainekset</Text>
+        <Header>Ainekset</Header>
         {[...ingredients]
             .map(({name, amount, unit}) =>
                 <Text>- {amount} {unit} {name}</Text>)}
         </View>
         <View style={{ paddingTop: 20 }}>
-            <Text>Valmistus</Text>
+            <Header>Valmistus</Header>
         {[...steps.map((step, i) => <Text>{i + 1}. {step}</Text>)]}
         </View>
     </View>)
