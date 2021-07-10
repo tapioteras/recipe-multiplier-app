9cb7bc57e8de48a4de93f088c6b007b0b5eacddb
Tapio Teras <tapio.teras@reaktor.com>
2021-07-10 12:20:16 +0300

    Implement bullet list (items)

--- src/screens/recipe.js
+++ src/screens/recipe.js
@@ -1,26 +1,27 @@
 import React, {useEffect, useState} from "react"
-import {Text, View} from "react-native";
+import {ScrollView, Text, View} from "react-native";
 import {parseKRuokaRecipe} from "../utils/RecipeUtils";
 import {styles} from "./recipes";
 import Header from "../components/Header";
+import BulletItem from "../components/BulletItem";
 
 const RecipeScreen = ({route}) => {
     const recipe = parseKRuokaRecipe(route.params.recipe)
 const { name, portions, ingredients, steps } = recipe
-    return (<View style={styles.container}>
+    return (<ScrollView style={styles.container}>
 <Header>{name}</Header>
         <Text>{portions} annosta</Text>
         <View style={{ paddingTop: 20 }}>
         <Header>Ainekset</Header>
         {[...ingredients]
             .map(({name, amount, unit}) =>
-                <Text>- {amount} {unit} {name}</Text>)}
+                <BulletItem>{amount} {unit} {name}</BulletItem>)}
         </View>
         <View style={{ paddingTop: 20 }}>
             <Header>Valmistus</Header>
-        {[...steps.map((step, i) => <Text>{i + 1}. {step}</Text>)]}
+        {[...steps.map((step, i) => <BulletItem>{i + 1}. {step}</BulletItem>)]}
         </View>
-    </View>)
+    </ScrollView>)
 }
 
 export default RecipeScreen
