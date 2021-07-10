847cd52b4dbd81e48a4b460707692465af632a81
Tapio Teras <tapio.teras@reaktor.com>
2021-07-10 11:47:14 +0300

    Parse recipes

--- src/utils/RecipeUtils.js
+++ src/utils/RecipeUtils.js
@@ -1,33 +1,18 @@
+import _ from "ramda"
 export const parseKRuokaRecipe = (
         recipeFromKRuoka
 ) => {
-    console.log("recipe from k-ruoka", recipeFromKRuoka)
     const {
         Name: name,
         UserPortions: { _: portions },
         Ingredients,
         Instructions,
     } = recipeFromKRuoka
-    const categories = Ingredients.map(({ SubSectionHeading: name }, id) => ({id, name}))
-
-    const ingredients = []
-    Ingredients
-        .forEach(({
-                  SubSectionIngredients: categoryIngredients,
-                  SubSectionHeading: categoryHeader }
-            ) => {
-            const [first] = categoryIngredients
-                first.map(({
-                          Name: name,
-                          Amount: amount,
-                          Unit: unit
-                      }) => ({
-                    name,
-                    amount,
-                    unit,
-                    categoryId: categories.find(({name}) => name === categoryHeader).id
-                }))
-        })
+    const categories = Ingredients
+        .filter(({SubSectionHeading}) => SubSectionHeading !== "_")
+        .map(({ SubSectionHeading: name }, id) => ({id, name}))
+    const ingredients = _.flatten(Ingredients.map(({SubSectionIngredients}) => _.flatten(SubSectionIngredients)))
+        .map(({ Name: name, Amount: amount, Unit: unit }) => ({ name, amount, unit }))
     return {
         name,
         portions,
