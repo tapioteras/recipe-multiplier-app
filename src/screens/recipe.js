6402acac7198fab4dcd89268d22da39d147961fd
Tapio Teras <tapio.teras@reaktor.com>
2021-07-10 12:00:20 +0300

    Fix recipe screen

--- src/screens/recipe.js
+++ src/screens/recipe.js
@@ -4,10 +4,20 @@ import {parseKRuokaRecipe} from "../utils/RecipeUtils";
 
 const RecipeScreen = ({route}) => {
     const recipe = parseKRuokaRecipe(route.params.recipe)
-const { name } = recipe
+const { name, portions, ingredients, steps } = recipe
     return (<View>
 <Text>{name}</Text>
-        <Text>{JSON.stringify(recipe)}</Text>
+        <Text>{portions} annosta</Text>
+        <View style={{ paddingTop: 20 }}>
+        <Text>Ainekset</Text>
+        {[...ingredients]
+            .map(({name, amount, unit}) =>
+                <Text>- {amount} {unit} {name}</Text>)}
+        </View>
+        <View style={{ paddingTop: 20 }}>
+            <Text>Valmistus</Text>
+        {[...steps.map((step, i) => <Text>{i + 1}. {step}</Text>)]}
+        </View>
     </View>)
 }
 
--- src/utils/RecipeUtils.js
+++ src/utils/RecipeUtils.js
@@ -4,20 +4,21 @@ export const parseKRuokaRecipe = (
 ) => {
     const {
         Name: name,
-        UserPortions: { _: portions },
+        UserPortions,
         Ingredients,
-        Instructions,
+        Instructions: steps,
     } = recipeFromKRuoka
     const categories = Ingredients
         .filter(({SubSectionHeading}) => SubSectionHeading !== "_")
         .map(({ SubSectionHeading: name }, id) => ({id, name}))
     const ingredients = _.flatten(Ingredients.map(({SubSectionIngredients}) => _.flatten(SubSectionIngredients)))
         .map(({ Name: name, Amount: amount, Unit: unit }) => ({ name, amount, unit }))
+    const {_: portions = 6} = UserPortions
     return {
         name,
         portions,
         categories,
-        Instructions,
+        steps: steps.split("# ").filter(s => !!s),
         ingredients
     }
 };
