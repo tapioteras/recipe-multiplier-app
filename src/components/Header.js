00343cba1996a3049a047c3c3e362adcc04fd02c
Tapio Teras <tapio.teras@reaktor.com>
2021-07-11 16:43:07 +0300

    Add support for h2 header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {Text} from "react-native";
 
-const Header = ({children}) =>
-    <Text style={{ fontSize: 24 }}>{children}</Text>
+const Header = ({children, level}) =>
+    <Text style={{paddingTop: 5, paddingBottom: 10, fontSize: level === 2 ? 25 : 32 }}>{children}</Text>
 
 export default Header
