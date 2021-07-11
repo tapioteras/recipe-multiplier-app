03966482b06a714dec9fc22e7b7677d3a207c45d
Tapio Teras <tapio.teras@reaktor.com>
2021-07-11 17:11:19 +0300

    Implement recipe multiplier feature
    having x2 and reset buttons

--- src/screens/recipe.js
+++ src/screens/recipe.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react"
 import {Button, ScrollView, Text, View} from "react-native";
-import {parseKRuokaRecipe} from "../utils/RecipeUtils";
+import {calculatePortion, parseKRuokaRecipe} from "../utils/RecipeUtils";
 import {styles} from "./recipes";
 import Header from "../components/Header";
 import BulletItem from "../components/BulletItem";
@@ -15,6 +15,8 @@ const RecipeScreen = ({route}) => {
             <Text>{portions} {portions === 1 ? "annos" : "annosta"}</Text>
             <Button onPress={() => setPortions(portions+1)} title="+" />
             <Button onPress={() => setPortions(portions < 2 ? portions : portions-1)} title="-" />
+            <Button onPress={() => setPortions(initialPortions * 2)} title="x2" />
+            <Button onPress={() => setPortions(initialPortions)} title="reset" />
         </View>
         <View style={{ paddingTop: 20 }}>
         <Header>Ainekset</Header>
@@ -22,6 +24,7 @@ const RecipeScreen = ({route}) => {
                 <Header level={2}>{categoryName}</Header>
                 {[...ingredients]
                     .filter(({category}) => categoryName == category)
+                    .map(({amount, ...rest}) => ({...rest, amount: calculatePortion({amount, originalPortion: initialPortions, multipliedPortion: portions})}))
                     .map(({name, amount, unit}) => <BulletItem>{amount} {unit} {name}</BulletItem>)}
             </View>)}
         </View>
