561f167b0125288781a84631a0a1d3682a99eace
Tapio Teras <tapio.teras@reaktor.com>
2021-07-24 22:36:51 +0300

    Overwrite global props

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 import "react-native-gesture-handler"
 import { registerRootComponent } from "expo"
+import {
+  setCustomView,
+  setCustomTextInput,
+  setCustomText,
+} from "react-native-global-props"
+
+setCustomText({
+  style: { color: "white" },
+})
+setCustomTextInput({ style: { color: "white" } })
+setCustomView({
+  style: { backgroundColor: "black" },
+})
 
 import App from "./App"
 
