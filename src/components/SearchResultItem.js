da4e1fee0e9f89a847d190725425f412e2efaffb
Tapio Teras <tapio.teras@reaktor.com>
2021-07-24 23:14:04 +0300

    Increase search result item font size

--- src/components/SearchResultItem.js
+++ src/components/SearchResultItem.js
@@ -2,10 +2,16 @@ import React from "react"
 import { Text, View } from "react-native"
 
 const SearchResultItem = (props) => (
-  <View style={{ flex: 1, flexDirection: "row", alignItems: "center" }}>
+  <View
+    style={{
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "center",
+      paddingVertical: 10,
+    }}>
     <Text
       {...props}
-      style={{ fontSize: 18, lineHeight: 30, paddingRight: 10, maxWidth: 300 }}
+      style={{ fontSize: 22, lineHeight: 30, paddingRight: 10, maxWidth: 300 }}
     />
     <View style={{ flex: 1, alignItems: "flex-end" }}>
       <Text style={{ fontSize: 30 }}>{">"}</Text>
