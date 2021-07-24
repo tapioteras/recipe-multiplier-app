7f3a4ff4be4f6580f97c0cf23519ed8386344ca7
Tapio Teras <tapio.teras@reaktor.com>
2021-07-24 22:53:40 +0300

    Set custom text input default props

--- index.js
+++ index.js
@@ -9,12 +9,22 @@ import {
 setCustomText({
   style: { color: "white" },
 })
-setCustomTextInput({ style: { color: "white" } })
+setCustomTextInput({
+  style: {
+    ...buttonStyles.button,
+    ...buttonStyles.text,
+    backgroundColor: "white",
+    color: "black",
+  },
+  placeholderTextColor: "grey",
+})
+
 setCustomView({
   style: { backgroundColor: "black" },
 })
 
 import App from "./App"
+import { styles as buttonStyles } from "./src/components/RecipeButton"
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
